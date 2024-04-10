1e5ded4b0378e9bf45c058fc8fd6e2c0550e521b
Christian Kuhtz <christian@kuhtz.com>
2024-04-09 20:24:48 -0700

    created periodic pskreporter pull and publish to redis pubsub

---
+++ pskreporter/README.md
@@ -0,0 +1,3 @@
+pull.py checks for periodic messages from redis pubsub and queries 
+PSKreporter for info for specific callsign.  this takes place no 
+less than every 5 minutes.
--- pskreporter/pull.py
+++ pskreporter/pull.py
@@ -12,21 +12,23 @@ import json
 import xmltodict
 import requests
 import redis
+import time
 
 
 callsign = "AK7VV"
 pskreporter_url = "https://retrieve.pskreporter.info/query?senderCallsign=" + callsign
 redis_host = "docker"
 redis_port = "6379"
+debug = True
 
 # wait for periodic message
 
-def wait_for_periodic(r, count):
+def wait_for_periodic(r, message_value, count):
     channel_name = 'periodic'
-    message_to_wait_for = json.dumps({ "type": "minute" })
+    message_to_wait_for = json.dumps({ "type": message_value })
 
     pubsub = r.pubsub()
-    pubsub.subscribe()
+    pubsub.subscribe(channel_name)
     
     message_count = 0
     while message_count < count:
@@ -34,25 +36,36 @@ def wait_for_periodic(r, count):
         if message and message['type'] == 'message':
             if message['data'].decode('utf-8') == message_to_wait_for:
                 message_count += 1
-        print("got {message_to_wait_for} for {message_count} out of {count} times.}")
-        time.sleep(30) # wait 10 seconds before checking again
+        if debug:
+            print(f"Received '{message_to_wait_for}' for {message_count} out of {count} times.")
+        if message_count < count:
+            time.sleep(20) # wait 20 seconds before checking again
 
+    if debug:
+        print ("wait is over.")
 
 # create Redis client
+
+if debug:
+    print("creating redis client")
 
 try:
     redis_client = redis.Redis(host=redis_host, port=redis_port)
 except Exception as e:
     print("Redis init problem:", str(e))
 
-# while True:
+while True:
 
     # wait for 5 minutes so we don't trip the rate limiting for PSKreporter
 
-    wait_for_periodic(redis_client, 5)
+    if debug:
+        print("waiting for 5 minutes..")
+    wait_for_periodic(redis_client, "minute", 5)
 
     # retrieve PSK reporter data for callsign as XML string
 
+    if debug:
+        print("getting PSKreporter data.")
     response = requests.get(pskreporter_url)
     xml_string = response.text
 
@@ -65,8 +78,10 @@ except Exception as e:
 
     try:
         redis_client.publish(
-            'periodic',
+            'pskreporter',
             json.dumps(pubsub_message)
         )
+        if debug:
+            print("Redis pubsub <<", pubsub_message)
     except Exception as e:
         print("Redis publish():", str(e))
