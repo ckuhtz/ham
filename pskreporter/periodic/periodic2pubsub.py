0ba774c9bfa568578ffad1dc7bde52a9c51fd346
Christian Kuhtz <christian@kuhtz.com>
2024-04-09 18:46:57 -0700

    create a periodic pubsub event. currently, this publishes once per minute. clients can listen to this event, count the minutes, and then do whatever they need to do.

---
+++ pskreporter/periodic/periodic2pubsub.py
@@ -0,0 +1,24 @@
+# 1. publish a periodic event on pubsub (e.g. 1 minute)
+
+import json
+import redis
+import time
+
+# create Redis client
+
+try:
+    redis_client = redis.Redis(host="docker", port=6379)
+except Exception as e:
+    print("Redis init problem:", str(e))
+
+periodic_message = {
+    "type": "minute"
+}
+
+while True:
+    redis_client.publish(
+        'periodic',
+        json.dumps(pubsub_message)
+    )
+
+    time.sleep(60)
